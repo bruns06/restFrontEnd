c3c3560d62028ee92b97ac62dd962820b0936ab3
bruns2506 <bruns2506@gmail.com>
2023-06-29 17:35:23 +0200

    first commit

---
+++ my-svelte-projec/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+// @ts-nocheck
+    // @ts-ignore
+    var data = []
+    fetch("http://localhost:5153/api/Marca")
+    .then(res => res.json())
+    .then(res => {console.log(res); data=res })
+
+   // export let data = [];
+
+    
+</script>
+
+{#each data as marche}
+    <h2>{marche.nomeMarca}</h2>
+{/each}
