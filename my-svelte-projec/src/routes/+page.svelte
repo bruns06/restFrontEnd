ff67233b6728a079df6f36813883dfed8b0ec75b
bruns2506 <bruns2506@gmail.com>
2023-06-29 17:58:53 +0200

    added cose

--- my-svelte-projec/src/routes/+page.svelte
+++ my-svelte-projec/src/routes/+page.svelte
@@ -2,12 +2,15 @@
 // @ts-nocheck
     // @ts-ignore
     var data = []
-    fetch("http://localhost:5153/api/Marca")
+    fetch("http://localhost:5153/api/Configurazione")
     .then(res => res.json())
-    .then(res => {console.log(res); data=res })
+    .then(res => {console.log(res); data=res})
    // export let data = [];
 </script>
 
-{#each data as marche}
-    <h2>{marche.nomeMarca}</h2>
+{#each data as confs}
+    <h2>{confs.name}</h2>
+    <h2>{confs.confId}</h2>
+    <h2>{confs.pezzi}</h2>
+    <h2>{confs.utenteId}</h2>
 {/each}
---
+++ my-svelte-projec/src/routes/homepage/+page.svelte
@@ -0,0 +1,27 @@
+<div class="wrapper"> 
+    <header class="site-header">
+      <nav>
+        | <a href="/creaConfs"><strong>Crea configurazione</strong></a> | 
+        <a href="/confs"><strong>Configurazione</strong></a> |
+        <a href="/login"><strong>Login</strong></a> |
+        <a href="/login"><strong>Register</strong></a> |
+      </nav>
+    </header>
+  
+    <main>
+      <div class="home-page">
+        <div class="block">
+          <h1>carDb</h1>
+          <p class="intro">Un carDb.</p>
+          <a href="/creaConfs" class="button">Crea configurazione</a>
+        </div>
+        <div class="block">
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/48935/hello.jpg" alt="An illustration of me in profile">
+        </div>
+      </div>
+    </main>
+  
+    <footer>
+      <small>Cose legali</small>
+    </footer>
+  </div>
